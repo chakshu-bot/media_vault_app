b253a198d7a585aab969a6e689391e49936aab0f
Aman Kumar Ajad <amankumarajad0@gmail.com>
2024-06-12 16:37:51 +0530

    jugaad lagane ki tayari

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'mediaPlayer.dart';
 import 'package:media_vault/screens/search_screen.dart';
 import 'package:media_vault/screens/convert_screen.dart';
 import 'package:media_vault/screens/download_screen.dart';
@@ -10,52 +9,12 @@ void main() {
   runApp(const MyApp());
 }
 
-// MyApp class definition
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Media Vault',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: HomeScreen(),
-      debugShowCheckedModeBanner: false,
-    );
-  }
-}
-
-// HomeScreen class (remains the same)
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("Media Vault"),),
-      body: Center(
-        child: Text("Our First Team Project 'Media Vault'"),
-      ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => DownloadedMediaPage()),
-          );
-        },
-        label: Text('Open Media Vault'),
-        icon: Icon(Icons.folder_open),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-    );
-  }
-}
-
       debugShowCheckedModeBanner: false,
       title: 'Media Vault',
       theme: ThemeData(
